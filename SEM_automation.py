7024382017d7fdd95a7b0e330c897364a49ae20a
Jyothi S <jyothishridhar0625@gmail.com>
2024-05-28 11:53:39 +0530

    Update requirements.txt

--- SEM_automation.py
+++ SEM_automation.py
@@ -57,7 +57,7 @@ def scrape_first_proper_paragraph(url):
         paragraph_count = 0
         for p in p_tags:
             paragraph = p.text.strip()
-            if len(paragraph) > 150:  # Check if the paragraph is not empty
+            if len(paragraph) > 100:  # Check if the paragraph is not empty
                 first_two_paragraphs_text += paragraph + ' '  # Add space between paragraphs
                 paragraph_count += 1
                 if paragraph_count == 2:  # Stop after finding the first two paragraphs
